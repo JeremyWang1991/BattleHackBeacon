20bdd7bc70910f3dd82324ee02f5147d22b3ccf4
Jeremy Wang <jeremywangtc@gmail.com>
2014-05-31 13:41:51 -0400

    Initial Commit

--- BattleHackBeacon/ViewController.m
+++ BattleHackBeacon/ViewController.m
@@ -17,7 +17,26 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    NSUUID *proximityUUID = [[NSUUID alloc]
+                             initWithUUIDString:@"2018631D-734B-4BD4-984F-1BD2A2FC5ADB"];
+    
+    // Create the beacon region.
+    CLBeaconRegion *beaconRegion = [[CLBeaconRegion alloc]
+                                    initWithProximityUUID:proximityUUID
+                                    identifier:@"com.mycompany.myregion"];
+    
+    // Create a dictionary of advertisement data.
+    NSDictionary *beaconPeripheralData =
+    [beaconRegion peripheralDataWithMeasuredPower:nil];
+    
+    // Create the peripheral manager.
+    CBPeripheralManager *peripheralManager = [[CBPeripheralManager alloc]
+                                              initWithDelegate:selfqueue:nil options:nil];
+    
+    // Start advertising your beacon's data.
+    [peripheralManager startAdvertising:beaconPeripheralData];
+
 }
 
 - (void)didReceiveMemoryWarning
