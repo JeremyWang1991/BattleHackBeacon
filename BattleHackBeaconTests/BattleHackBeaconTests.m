5562ea92a25321219b8a09de497e5ad747af976e
Jeremy Wang <jeremywangtc@gmail.com>
2014-05-31 11:53:36 -0400

    Initial Commit

---
+++ BattleHackBeaconTests/BattleHackBeaconTests.m
@@ -0,0 +1,34 @@
+//
+//  BattleHackBeaconTests.m
+//  BattleHackBeaconTests
+//
+//  Created by Tse-Chi Wang on 5/31/2014.
+//  Copyright (c) 2014 Jeremy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BattleHackBeaconTests : XCTestCase
+
+@end
+
+@implementation BattleHackBeaconTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
